e19e6da23a0862a51b11eb46e482402d30d929b7
Sanjik761 <sanjarburiniyozov@gmail.com>
2023-10-07 13:50:30 +0500

    Initial commit

---
+++ seminar2task4.py
@@ -0,0 +1,4 @@
+cw_mark = input("Enter your Coursework mark: ")
+exam_mark = input("Enter your exam mark: ")
+total = (int(cw_mark) * 0.4) + (int(exam_mark) * 0.6)
+print(f'You got overall {total} mark in CSFun')
